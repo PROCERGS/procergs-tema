f249335f5ee0fb0b56be099551dc8d75d9f9f974
henrique-saenger <henrique-saenger@procergs.rs.gov.br>
2026-08-03 14:32:00 -0300

    #1429855- Criação de uma linha de prioridade para título, logo, etc, visando incorporação futura de subsites

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,4 +1,18 @@
 const configureSettings = (config) => {
+  config.settings.navDepth = 2;
+
+  config.settings.apiExpanders = [
+    ...(config.settings.apiExpanders || []),
+    {
+      match: '',
+      GET_CONTENT: ['inherit'],
+      querystring: {
+        'expand.inherit.behaviors':
+          'procergs.sitebase.header,voltolighttheme.header',
+      },
+    },
+  ];
+
   config.settings.procergsFooter = {
     navigationLabel: 'Rodapé principal',
     asidePosition: 'after',
